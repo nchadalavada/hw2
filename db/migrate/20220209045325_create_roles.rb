febd45ce64119b55b1b2735574d1c468b307bfc6
nchadalavada <naveen.chadalavada@kellogg.northwestern.edu>
2022-02-09 04:55:21 +0000

    commit

--- db/migrate/20220209045325_create_roles.rb
+++ db/migrate/20220209045325_create_roles.rb
@@ -1,8 +1,9 @@
 class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
-      t.string :character_name
-      
+      t.string : character_name
+      t.string : movie
+      t.string : person
 
       t.timestamps
     end
